7072d04558458ccb12830e0db205ab67dc485d08
yair-dardik <yairdardik@gmail.com>
2025-12-06 19:56:45 +0200

    bye bye

--- commands.c
+++ commands.c
@@ -5,7 +5,7 @@
 //example function for printing errors from internal commands
 void perrorSmash(const char* cmd, const char* msg)
 {
-    fprintf(stderr, "smash error:%s%s%s\n",
+    fprintf(stderr, "smash error: %s%s%s\n",
         cmd ? cmd : "",
         cmd ? ": " : "",
         msg);
