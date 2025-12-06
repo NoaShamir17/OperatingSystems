13d0cc40473fa31d00442f41ab192bd8e6483b43
yair-dardik <yairdardik@gmail.com>
2025-12-06 16:54:56 +0200

    pls5

--- signals.c
+++ signals.c
@@ -1,2 +1,13 @@
 // signals.c
 #include "signals.h"
+#include "signals.h"
+
+// Triggered by Ctrl+Z (SIGTSTP)
+void ctrlZHandler(int sig_num) {
+    // TODO: Implement logic to catch SIGTSTP and stop foreground process
+}
+
+// Triggered by Ctrl+C (SIGINT)
+void ctrlCHandler(int sig_num) {
+    // TODO: Implement logic to catch SIGINT and kill foreground process
+}
