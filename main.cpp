af473b3cad0adfd5d837613bba402cfe0bccdde9
ItaySamuelov <itaysamuelov@campus.technion.ac.il>
2026-01-05 14:13:32 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "Bank.h"
+#include "ATM.h"
+#include <pthread.h>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // Minimal stub to validate linking; real implementation should parse args and start threads
+    (void)argc; (void)argv;
+    return 0;
+}
